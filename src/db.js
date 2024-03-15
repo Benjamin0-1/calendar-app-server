fd64ed303a376db92baeeb1196d4284271f078cf
Oliver <oliver@MacBook-Air.local>
2024-03-15 02:37:43 -0600

    updated db

--- src/db.js
+++ src/db.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require('sequelize');
 
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false, // Disable logging to avoid cluttering the console
+  });
+  
+  // Set the timezone if needed
+  sequelize.options.timezone = 'America/Mexico_City';
+  
+  module.exports = sequelize;
+
+ /*
 const sequelize = new Sequelize({
     database: 'Terraza',
     username: 'postgres',
-    password: '', // 
+    password: 'postgres', // was 12******
     host: 'localhost',
     dialect: 'postgres',
     logging: false
@@ -12,3 +25,5 @@ const sequelize = new Sequelize({
 sequelize.options.timezone = 'America/Mexico_City';
 
 module.exports = sequelize;
+
+*/
