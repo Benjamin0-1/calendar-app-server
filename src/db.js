7a1834895fed28cb08bb5dc6af3144622bcaa36b
Oliver <oliver@MacBook-Air.local>
2024-03-23 04:18:06 -0600

    asd

--- src/db.js
+++ src/db.js
@@ -63,9 +63,14 @@ const sequelize = new Sequelize({
   username: 'utc4n60886t2p',
   password: 'p2c1cc0a3788525f9eb31b95be499aa1b9b4df76d06af3ad4f35daf4742c188aa',
   host: 'cb889jp6h2eccm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-  ssl: true,
-  logging: false,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will enforce SSL
+      rejectUnauthorized: false // This option bypasses the verification of the certificate
+    }
+  },
+  logging: false,
 });
 
 sequelize.options.timezone = 'America/Mexico_City';
