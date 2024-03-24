2a2216850628f11b9902ce32b8ae7cc68bcb81c1
Oliver <oliver@MacBook-Air.local>
2024-03-24 04:36:34 -0600

    cors fix

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const session = require('express-session');
 const app = express();
 
 const corsOptions = {
-    origin: ['https://65feb9f2159c7fed5acec402--rad-otter-086d62.netlify.app', 'http://localhost:3001', '*'],
+    origin: ['https://rad-otter-086d62.netlify.app'],
     credentials: true,
-    methods: ['POST', 'PUT', 'DELETE'] // Apply CORS to all methods except GET
+    methods: ['POST', 'DELETE', 'PUT'] // Apply CORS to all methods except GET
 };
 
 
